16ea7a5c758a359bd09527bbbdb21e6656ce14f6
vidhirkaup <vidhir.kaup@gmail.com>
2021-04-09 18:56:08 -0400

    Initial commit

---
+++ src/main/java/com/vlabs/statemachine/HwStatemachineApplication.java
@@ -0,0 +1,13 @@
+package com.vlabs.statemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwStatemachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwStatemachineApplication.class, args);
+	}
+
+}
